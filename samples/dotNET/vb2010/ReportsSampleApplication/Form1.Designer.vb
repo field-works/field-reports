f4bb3feb8ccf4c0ac849abccd1b121ce231f4e5d
Kenji Kajita <kajita@field-works.co.jp>
2015-06-17 15:06:56 +0900

    C#とVB.NETのサンプルを追加

---
+++ samples/dotNET/vb2010/ReportsSampleApplication/Form1.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(50, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "メッセージ"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(70, 5)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(180, 19)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "Hello World!"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(175, 87)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "帳票生成"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 45)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 12)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "ファイル名"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(70, 42)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(180, 19)
+        Me.TextBox2.TabIndex = 4
+        Me.TextBox2.Text = "test.pdf"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(264, 135)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Field Reports Sample Application"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+
+End Class
---
+++ samples/dotNET/vb2010/ReportsSampleApplication/Form1.vb
@@ -0,0 +1,22 @@
+﻿Imports Field
+
+Public Class Form1
+
+    Private paramTemplate = "{{" & _
+"    ""template"": {{""paper"": ""A4""}}," & _
+"    ""context"": {{" & _
+"        ""hello"": {{" & _
+"            ""new"": ""Tx""," & _
+"            ""value"": ""{0}""," & _
+"            ""rect"": [100, 700, 400, 750]" & _
+"        }}" & _
+"    }}" & _
+"}}"
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim reports As New Reports
+        Dim param As String = String.Format(paramTemplate, TextBox1.Text)
+        reports.Render(param, TextBox2.Text)
+    End Sub
+
+End Class
